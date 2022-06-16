7f309c6ad8716dce66bc5024e78b1886f4da533b
Jeffrey Albion <jeffrey.albion@outlook.com.au>
2022-06-16 22:57:15 +1000

    working

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT ?? 3000;
+const fs = require('fs');
+var path = require('path');
+
+files = fs.readdirSync("./images")
+console.log(files)
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+      var options = {
+        root: path.join(__dirname,"images")
+    };
+
+    file = getRandomInt(files.length)
+    console.log(file)
+    var fileName = files[file];
+    res.sendFile(path.join(fileName), options, function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 })
 
 app.listen(port, () => {
